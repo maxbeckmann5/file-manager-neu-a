62cd48617d47b8edffe76e63781735428781bef9
maxbkm <133896361+maxbkm@users.noreply.github.com>
2023-11-15 14:39:00 +0100

    added html+css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FileComponent } from './file/file.component';
+import { ManageComponent } from './manage/manage.component';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'file', component: FileComponent },
+  { path: 'manage', component: ManageComponent},
+  { path: 'teams', component: TeamsComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FileComponent } from './file/file.component';
+import { ManageComponent } from './manage/manage.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FileComponent,
+    ManageComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
